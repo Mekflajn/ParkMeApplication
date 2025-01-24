ba5df733ce8fcc953186ade2dc801aa4b92b6742
Stefan77887 <stefanmarinkovic778@gmail.com>
2025-01-24 22:26:22 +0100

    jajajaja

---
+++ Screens/OpisLokacije1.js
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const OpisLokacije1 = () => {
+  return (
+    <View>
+      <Text>OpisLokacije1</Text>
+    </View>
+  )
+
+}
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        
+    }
+})
+
+export default OpisLokacije1
